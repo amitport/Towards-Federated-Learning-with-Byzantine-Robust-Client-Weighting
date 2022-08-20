133c9e18be16ef663f3c976e3ebf20cdd6626f2a
yoavti <yoavti@post.bgu.ac.il>
2022-08-20 22:25:11 +0300

    Added client_weight.py

---
+++ experiments/shakespeare/client_weight.py
@@ -0,0 +1,22 @@
+import tensorflow_federated as tff
+
+
+DATASET_MODULES = {'cifar100': tff.simulation.datasets.cifar100,
+                   'emnist': tff.simulation.datasets.emnist,
+                   'gldv2': tff.simulation.datasets.gldv2,
+                   'experiments': tff.simulation.datasets.shakespeare,
+                   'stackoverflow': tff.simulation.datasets.stackoverflow}
+
+
+def extract_weights(client_datasets):
+  listed = (list(client_dataset) for client_dataset in client_datasets)
+  weights = (len(client_dataset) for client_dataset in listed)
+  return weights
+
+
+def get_client_weights(name, limit_count=None):
+  module = DATASET_MODULES[name]
+  dataset = module.load_data()[0]
+  client_datasets = dataset.datasets(limit_count=limit_count)
+  weights = extract_weights(client_datasets)
+  return weights
